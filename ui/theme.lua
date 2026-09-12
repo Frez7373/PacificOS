805488b212c4412bd7007b9f67d3c54db429aee5
Frez7373 <danilla.mikhailov@gmail.com>
2026-09-12 17:39:51 +0300

    Add PacificOS theme

---
+++ ui/theme.lua
@@ -0,0 +1,4 @@
+return {
+ bg=colors.black, panel=colors.blue, panel2=colors.lightBlue, text=colors.white, muted=colors.lightGray,
+ accent=colors.cyan, good=colors.lime, warn=colors.yellow, bad=colors.red, dark=colors.gray
+}
